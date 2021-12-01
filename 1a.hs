46594e0677e3d89aac12fc2dbf7301f9f9fdb7ac
Mark Hegreberg <Mark@hegreberg.io>
2021-11-30 22:52:30 -0800

    day one, challenge A

---
+++ 1a.hs
@@ -0,0 +1,24 @@
+{-
+Advent of Code
+Mark Hegreberg
+1a - find number of times depth increases
+-}
+
+sampleData = [199,200,208,210,200,207,240,269,260,263]
+
+inputData = readFile "./1.input"
+
+main = do
+       contents <-readFile "./1.input"
+       let input = lines contents
+           answer = solution input
+       print answer
+
+
+solution input = length [x | x <- increaseCheck input, x == GT]
+
+increaseCheck :: (Ord a) => [a] ->[Ordering]
+increaseCheck [] = [EQ]
+increaseCheck [_] = [EQ]
+increaseCheck [x, y] = [compare y x]
+increaseCheck (x:y:ys) = compare y x:increaseCheck (y:ys)
---
+++ scratch.hs
@@ -0,0 +1,26 @@
+
+tellLate :: (Num a, Ord a) => a -> String
+tellLate time
+        | time  <  0 = "You're actually early"
+        | time ==  0 = "You're on time!"
+        | time <= 15 = "Basically on time"
+        | time <= 30 = "Traffic?"
+        | otherwise  = "Wow, you're actually late"
+
+fibbonacci :: (Integral a) =>a -> a
+fibbonacci 0 = 1
+fibbonacci 1 = 1
+fibbonacci n = fibbonacci (n-1) + fibbonacci (n-2)
+
+
+isIncreased :: (Ord a) => [a] -> Ordering
+isIncreased [] = EQ
+isIncreased [_] = EQ
+isIncreased [x, y] = compare y x
+
+
+-- increaseCheck :: (Ord a) => [a] ->[Ordering]
+increaseCheck [] = [EQ]
+increaseCheck [_] = [EQ]
+increaseCheck [x, y] = [compare y x]
+increaseCheck (x:y:ys) = compare y x:increaseCheck (y:ys)
