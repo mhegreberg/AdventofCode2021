4057c0ab1d4991a7a025a46709b81373e5cb5dcd
Mark Hegreberg <Mark@hegreberg.io>
2021-12-03 18:39:31 -0800

    3b works with test data, but not with real data for some reason?
    value is too high

--- 3b.hs
+++ 3b.hs
@@ -1,7 +1,7 @@
 {-
 Advent of Code
 Mark Hegreberg
-2a - calculate final position from submarine headings
+3b - gather diagnostic data about O2 and CO2 from binary data
 -}
 
 sampleData = [00100,
@@ -26,17 +26,27 @@ main = do
    contents <- readFile "./3.input"
    let input = lines contents
        input' = map (read::String->Int) input
-   print $ solution 12 input'
+   print $ solution 11 input'
 
 
-solution width x = 0
-
+solution width x = toDec' (filterOx width x) * toDec' (filterCO2 width x)
 filterOx :: Int -> [Int] -> [Int]
 filterOx 0 x = x
-filterOx width x 
+filterOx _ [x] = [x]
+filterOx width x
         | com >= 0   = filterOx (width-1) (filter (isPlace place 1) x)
         | otherwise = filterOx (width-1) (filter (isPlace place 0) x)
-        where 
+        where
+              place = 10 ^ (width-1)
+              com = common $ getPlaces place x
+
+filterCO2 :: Int -> [Int] -> [Int]
+filterCO2 0 x = x
+filterCO2 _ [x] = [x]
+filterCO2 width x
+        | com >= 0   = filterCO2 (width-1) (filter (isPlace place 0) x)
+        | otherwise = filterCO2 (width-1) (filter (isPlace place 1) x)
+        where
               place = 10 ^ (width-1)
               com = common $ getPlaces place x
 
@@ -57,10 +67,14 @@ getPlace place x
 getPlaces :: (Integral a) => a -> [a] -> [a]
 getPlaces place = map (getPlace place)
 
-isPlace place val x 
+isPlace place val x
         | getPlace place x == val = True
         | otherwise = False
 
 
-toDec :: [Integer] -> Integer
-toDec input = foldr (\x y ->x + 2*y) 0 $ reverse input
+toDec :: Integral a => a -> a
+toDec 0 = 0
+toDec x = 2 * toDec (div x 10) + mod x 10
+
+toDec' :: Integral a => [a] -> a
+toDec' [x] = toDec x
